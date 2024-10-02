f7a366e20c797839a217f95443f4f1f0212d59e5
Gopinath <114890539+gopinathpriya24@users.noreply.github.com>
2024-10-02 16:59:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,8 +25,8 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+Rzd_odsTBFgwOT
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001921446982').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gopinath2483:UHDNewDB@clusternew.f2mfa.mongodb.net/?retryWrites=true&w=majority&appName=ClusterNew")
-DATABASE_NAME = environ.get('DATABASE_NAME', "Clusternew")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gopinath2483:jisshurepouhd@cluster0.ic08t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
 #---------------------------------------------------------------
